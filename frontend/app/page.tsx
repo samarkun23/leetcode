777c4d984eef9aa124f1fe38142b270ec4aeab53
samarkun <samarkun4@gmail.com>
2025-12-28 09:11:17 -0500

    signin auth

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,25 +1,60 @@
 "use client"
 import { useEffect } from "react";
 import { Landing } from "./component/Landing";
-import {onAuthStateChanged} from 'firebase/auth'
+import { onAuthStateChanged } from 'firebase/auth'
 import { auth } from "@/utils/firebase";
+import { RecoilRoot, useRecoilState, useSetRecoilState } from "recoil";
+import { userAtom } from "@/store/atoms/user";
+import { useRouter } from "next/navigation";
+
 
 
 export default function Home() {
+  return <StoreApp />
+}
 
+function StoreApp() {
+  const [user, setUser] = useRecoilState(userAtom)
+  const router = useRouter();
+  
   useEffect(() => {
-    const unsub = onAuthStateChanged(auth , function(user) {
-      if(user){
-        console.log("This is the user: ", user)
-      }else{
+    const unsub = onAuthStateChanged(auth, function (user) {
+      if (user && user.email) {
+        setUser({
+          loading: false,
+          user: {
+            email: user.email
+          }
+        })
+      } else {
+        setUser({
+          loading: false
+        })
         console.log("There is no logged in user");
       }
     });
 
     return () => unsub()
-  },[])
+  }, [])
 
+  useEffect(() => {
+    if (!user.loading && !user.user) {
+      router.push("/signin"); // 👈 no BACKEND_URL
+    }
+  }, [user.loading, user.user, router]);
+
+  if (user.loading) {
+    return <div>
+      loading ...
+    </div>
+  }
+
+  if (!user.user) {
+    return null
+  }
   return (
-    <Landing />
+    <div>
+      you logged in as {user.user?.email}
+    </div>
   );
 }
---
+++ frontend/next.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  // any other Next.js options...
+};
+
+module.exports = nextConfig;
